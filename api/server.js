c1e08e12f4cbb6e6df5fd35b991e31f3837e5e6f
Imon Ovbude <imonovbude@gmail.com>
2019-03-15 08:37:48 -0700

    created 3 get request test

--- api/server.js
+++ api/server.js
@@ -8,4 +8,10 @@ let games = [
     { id: 1, title: 'dragon ball fighter', genre: 'fighting' }
 ]
 
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Server Running'
+    })
+})
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -11,4 +11,22 @@ describe('server Get /', () => {
             })
             .catch()
     });
+
+    it('should return {message: "Server Running"}', () => {
+        return request(server)
+            .get('/')
+            .then(res => {
+                expect(res.body).toEqual({ message: 'Server Running' })
+            })
+            .catch()
+    })
+
+    it('should return 200 ok', () => {
+        return request(server)
+            .get('/games')
+            .then(res => {
+                expect(res.status).toBe(200)
+            })
+            .catch()
+    })
 });

313d77e5a0d7b3a5e6cb2daa2f5be201ca498f66
Imon Ovbude <imonovbude@gmail.com>
2019-03-15 08:56:29 -0700

    created get request for games to pass test

--- api/server.js
+++ api/server.js
@@ -8,10 +8,15 @@ let games = [
     { id: 1, title: 'dragon ball fighter', genre: 'fighting' }
 ]
 
+// GET REQUEST
 server.get('/', (req, res) => {
     res.status(200).json({
         message: 'Server Running'
     })
+})
+
+server.get('/games', (req, res) => {
+    return res.status(200).json(games)
 })
 
 module.exports = server;
